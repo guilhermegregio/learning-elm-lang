c64691fd64e978b5588d2ec55589470967925355
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2018-04-12 20:09:32 -0300

    add http get string sample

---
+++ beginning-elm/elm-examples/HttpExamples.elm
@@ -0,0 +1,124 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Http
+
+
+type alias Model =
+    { nicknames : List String
+    , errorMessage : Maybe String
+    }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick SendHttpRequest ]
+            [ text "Get data from server" ]
+        , viewNicknamesOrError model
+        ]
+
+
+viewNicknamesOrError : Model -> Html Msg
+viewNicknamesOrError model =
+    case model.errorMessage of
+        Just message ->
+            viewError message
+
+        Nothing ->
+            viewNicknames model.nicknames
+
+
+viewError : String -> Html Msg
+viewError errorMessage =
+    let
+        errorHeading =
+            "Couldn't fetch nicknames at this time."
+    in
+        div []
+            [ h3 [] [ text errorHeading ]
+            , text ("Error: " ++ errorMessage)
+            ]
+
+
+viewNicknames : List String -> Html Msg
+viewNicknames nicknames =
+    div []
+        [ h3 [] [ text "Old School Main Characters" ]
+        , ul [] (List.map viewNickname nicknames)
+        ]
+
+
+viewNickname : String -> Html Msg
+viewNickname nickname =
+    li [] [ text nickname ]
+
+
+type Msg
+    = SendHttpRequest
+    | DataReceived (Result Http.Error String)
+
+
+url : String
+url =
+    "http://localhost:8080/old-school.txt"
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SendHttpRequest ->
+            ( model, Http.send DataReceived (Http.getString url) )
+
+        DataReceived (Ok nicknamesStr) ->
+            let
+                nicknames =
+                    String.split "," nicknamesStr
+            in
+                ( { model | nicknames = nicknames }, Cmd.none )
+
+        DataReceived (Err httpError) ->
+            ( { model
+                | errorMessage = Just (createErrorMessage httpError)
+              }
+            , Cmd.none
+            )
+
+
+createErrorMessage : Http.Error -> String
+createErrorMessage httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "It appears you don't have an Internet connection right now."
+
+        Http.BadStatus response ->
+            response.status.message
+
+        Http.BadPayload message response ->
+            message
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { nicknames = []
+      , errorMessage = Nothing
+      }
+    , Cmd.none
+    )
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
