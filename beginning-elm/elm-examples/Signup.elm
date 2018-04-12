d27d02d13ad122c1f19db3bd9dde540c98f82c55
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2018-04-12 11:50:00 -0300

    Play with style in line

--- beginning-elm/elm-examples/Signup.elm
+++ beginning-elm/elm-examples/Signup.elm
@@ -24,13 +24,14 @@ initialModel =
 view : User -> Html msg
 view user =
     div []
-        [ h1 [] [ text "Sign up" ]
-        , Html.form []
+        [ h1 [ headerStyle ] [ text "Sign up" ]
+        , Html.form [ formStyle ]
             [ div []
                 [ text "Name"
                 , input
                     [ id "name"
                     , type_ "text"
+                    , inputTextStyle
                     ]
                     []
                 ]
@@ -39,6 +40,7 @@ view user =
                 , input
                     [ id "email"
                     , type_ "email"
+                    , inputTextStyle
                     ]
                     []
                 ]
@@ -47,15 +49,60 @@ view user =
                 , input
                     [ id "password"
                     , type_ "password"
+                    , inputTextStyle
                     ]
                     []
                 ]
             , div []
                 [ button
-                    [ type_ "submit" ]
+                    [ type_ "submit"
+                    , buttonStyle
+                    ]
                     [ text "Create my account" ]
                 ]
             ]
+        ]
+
+
+headerStyle : Attribute msg
+headerStyle =
+    style
+        [ ( "padding-left", "3cm" ) ]
+
+
+formStyle : Attribute msg
+formStyle =
+    style
+        [ ( "border-radius", "5px" )
+        , ( "background-color", "#f2f2f2" )
+        , ( "padding", "20px" )
+        , ( "width", "300px" )
+        ]
+
+
+inputTextStyle : Attribute msg
+inputTextStyle =
+    style
+        [ ( "display", "block" )
+        , ( "width", "260px" )
+        , ( "padding", "12px 20px" )
+        , ( "margin", "8px 0" )
+        , ( "border", "none" )
+        , ( "border-radius", "4px" )
+        ]
+
+
+buttonStyle : Attribute msg
+buttonStyle =
+    style
+        [ ( "width", "300px" )
+        , ( "background-color", "#397cd5" )
+        , ( "color", "white" )
+        , ( "padding", "14px 20px" )
+        , ( "margin-top", "10px" )
+        , ( "border", "none" )
+        , ( "border-radius", "4px" )
+        , ( "font-size", "16px" )
         ]
 
 
