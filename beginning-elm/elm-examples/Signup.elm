7d1789832c97e43ab3d5f58d0bf551e79e482a2a
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2018-04-12 18:48:01 -0300

    Add feature for update

--- beginning-elm/elm-examples/Signup.elm
+++ beginning-elm/elm-examples/Signup.elm
@@ -4,12 +4,12 @@ import Css exposing (..)
 import Html
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (id, type_, css, href, src)
-import Html.Styled.Events exposing (onClick)
+import Html.Styled.Events exposing (onClick, onInput)
 import SignupStyle exposing (..)
 
 
-view : Model -> Html Msg
-view model =
+view : User -> Html Msg
+view user =
     div []
         [ styledH1 [] [ text "Sign up" ]
         , styledForm []
@@ -18,6 +18,7 @@ view model =
                 , inputText
                     [ id "name"
                     , type_ "text"
+                    , onInput SaveName
                     ]
                     []
                 ]
@@ -26,6 +27,7 @@ view model =
                 , inputText
                     [ id "email"
                     , type_ "email"
+                    , onInput SaveEmail
                     ]
                     []
                 ]
@@ -34,18 +36,23 @@ view model =
                 , inputText
                     [ id "password"
                     , type_ "password"
+                    , onInput SavePassword
                     ]
                     []
                 ]
             , div []
-                [ styledButton [ type_ "submit" ]
+                [ styledButton
+                    [ type_ "submit"
+                    , onClick Signup
+                    ]
                     [ text "Create my account" ]
                 ]
             ]
+        , code [] [ text (toString user) ]
         ]
 
 
-main : Program Never Model Msg
+main : Program Never User Msg
 main =
     Html.beginnerProgram
         { view = view >> toUnstyled
@@ -54,19 +61,41 @@ main =
         }
 
 
-update : Msg -> Model -> Model
-update msg model =
-    model
+type alias User =
+    { name : String
+    , email : String
+    , password : String
+    , loggedIn : Bool
+    }
 
 
 type Msg
-    = DoSomething
+    = SaveName String
+    | SaveEmail String
+    | SavePassword String
+    | Signup
 
 
-type alias Model =
-    ()
+update : Msg -> User -> User
+update message user =
+    case message of
+        SaveName name ->
+            { user | name = name }
+
+        SaveEmail email ->
+            { user | email = email }
+
+        SavePassword password ->
+            { user | password = password }
+
+        Signup ->
+            { user | loggedIn = True }
 
 
-initialModel : Model
+initialModel : User
 initialModel =
-    ()
+    { name = ""
+    , email = ""
+    , password = ""
+    , loggedIn = False
+    }
